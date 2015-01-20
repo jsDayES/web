1b90e2e7092e678284c7196df8d5b9f43ddfd42d
Juanfran <juanfran.ag@gmail.com>
2015-01-20 19:02:17 +0100

    init menu

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,15 @@ var jade = require('gulp-jade');
 var plumber = require("gulp-plumber");
 var sass = require("gulp-ruby-sass");
 var scsslint = require("gulp-scss-lint");
+var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
 
 var paths = {
   mainJade: 'src/templates/index.jade',
   jade: 'src/templates/**/*.jade',
   mainSass: 'src/styles/main.scss',
   sass: 'src/styles/**/*.scss',
+  images: 'src/images/**/*',
   dist: 'dist/'
 };
 
@@ -27,16 +30,25 @@ gulp.task('express', function() {
   app.listen(9001);
 });
 
-gulp.task("sass-lint", function() {
-  gulp.src(paths.mainSass)
-    .pipe(scsslint({config: "scsslint.yml"}))
+gulp.task("copy-images", function() {
+  gulp.src(paths.images)
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest(paths.dist + '/images/'));
 });
 
-gulp.task("sass", ["sass-lint"], function() {
+gulp.task('sass-lint', function() {
+  gulp.src([paths.sass, '!src/styles/reset.scss'])
+    .pipe(scsslint({config: 'scsslint.yml'}))
+});
+
+gulp.task('sass', ['sass-lint'], function() {
   gulp.src(paths.mainSass)
     .pipe(plumber())
     .pipe(sass({
       'sourcemap=none': true
+    }))
+    .pipe(autoprefixer({
+      cascade: false
     }))
     .pipe(gulp.dest(paths.dist + 'styles/'));
 });
@@ -54,6 +66,7 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', [
+  'copy-images',
   'sass',
   'jade',
   'express',
